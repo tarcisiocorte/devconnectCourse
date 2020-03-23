13964f1de1608aa773a333c27e0091033b66167d
Tarcísio Corte <tcorte@epayworldwide.com.br>
2020-03-23 07:16:12 -0300

    create API

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
