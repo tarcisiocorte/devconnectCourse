e42ff2a9bc242e27f6f3e40bce74ab313f96065d
Tarcisio Corte <tarcisiocorte@gmail.com>
2020-03-30 06:32:55 -0300

    user registration api has been done

--- config/db.js
+++ config/db.js
@@ -6,11 +6,14 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useCreateIndex: true,
       useUnifiedTopology: true
     });
+
     console.log('MongoDB Connected...');
   } catch (err) {
-    console.log(err.message);
+    console.error(err.message);
+    // Exit process with failure
     process.exit(1);
   }
 };
